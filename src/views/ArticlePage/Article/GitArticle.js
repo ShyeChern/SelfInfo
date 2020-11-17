a79107339e66823f5105d461c32c558aa242b1c3
Shye Chern Lim <46220892+ShyeChern@users.noreply.github.com>
2020-11-17 20:31:08 +0800

    update

--- src/views/ArticlePage/Article/GitArticle.js
+++ src/views/ArticlePage/Article/GitArticle.js
@@ -115,7 +115,7 @@ export default function GitArticle(props) {
                 <CardBody>
                   <GridContainer style={{ textAlign: 'center' }} justify="center">
                     <GridItem xs={12} sm={12} md={12}>
-                      <p className={article.date}>Last update on 14 Nov 2020</p>
+                      <p className={article.date}>Last update on 15 Nov 2020</p>
                     </GridItem>
 
                     <GridItem xs={12} sm={12} md={10}>
@@ -145,23 +145,25 @@ export default function GitArticle(props) {
                       <p className={article.text}>
                         <b>Explanation: </b>
                         Add a new connection to a remote repository. After adding a remote, you can use [name] as
-                        a shortcut for [url] in other commands. For example: git remote add origin <b>url</b> and follow by
-                        git pull <b>origin</b> master
+                        a shortcut for [url] in other commands. For example: <b><i>git remote add origin [url]</i></b> and follow by 
+                        <b><i> git pull origin master</i></b>
                       </p>
                       <p className={article.highlightText} style={style.highlight}>
                         <b>git add [directory]</b>
                       </p>
                       <p className={article.text}>
                         <b>Explanation: </b>
-                        Stage all changes in [directory] for the next commit. Replace [directory] with a [filename] to
-                        change a specific file. If period is used (git add <b>.</b>) It will stage all changes in the directory
+                        Stage all changes in [directory] for the next commit. Replace <b><i>[directory]</i></b> with 
+                        a <b><i>[filename]</i></b> to change a specific file. If period is used (<b><i>git add .</i></b>) It will stage 
+                        all changes in the current 
+                        directory
                       </p>
                       <p className={article.highlightText} style={style.highlight}>
                         <b>git commit -m "[message]"</b>
                       </p>
                       <p className={article.text}>
                         <b>Explanation: </b>
-                        Commit the staged changes with "[message]" as the commit message.
+                        Commit the staged changes with <b><i>[message]</i></b> as the commit message.
                       </p>
                       <p className={article.highlightText} style={style.highlight}>
                         <b>git push [remote] [branch]</b>
@@ -169,7 +171,8 @@ export default function GitArticle(props) {
                       <p className={article.text}>
                         <b>Explanation: </b>
                         Push the commit into live and merge with the remote branch. For example,
-                        git push origin master will push all your changes in local repository and merge with remote master branch.
+                        <b><i>git push origin master</i></b> will push all your changes in local repository and merge with remote 
+                        master branch.
                       </p>
                       <p className={article.highlightText} style={style.highlight}>
                         <b>git pull [remote] [branch]</b>
@@ -177,131 +180,44 @@ export default function GitArticle(props) {
                       <p className={article.text}>
                         <b>Explanation: </b>
                         Fetch the specified remote update and immediately merge it into the local copy. For example,
-                        git pull origin master will update your local repository with the latest one in the remote master branch.
+                        <b><i> git pull origin master</i></b> will update your local repository with the latest one in the remote 
+                        master branch.
                       </p>
                     </GridItem>
 
                     <GridItem xs={12} sm={12} md={10}>
                       <hr />
                       <h3>Git Advanced Command</h3>
-                      <p className={article.text} style={style.highlight}>
-                        Two types of separated date and time format, default one and the locale one
+                      <p className={article.highlightText} style={style.highlight}>
+                        <b>git branch </b>
                       </p>
-                      <center>
-                        <GridItem xs={12} sm={12} md={10}>
-                          <Gist id="4981cff6cb21818814e5b7c435d0b005" file="DateAndTime.js" />
-                        </GridItem>
-                      </center>
-                    </GridItem>
-
-                    <GridItem xs={12} sm={12} md={10}>
-                      <hr />
-                      <h3>Modifying Locale DateTime Format</h3>
-                      <p className={article.text} style={style.highlight}>
-                        There is a second parameter accept by the locale format which allow you to modifying
-                        the datetime format. (Also for date format and time format)
+                      <p className={article.text}>
+                        <b>Explanation: </b>
+                        List down all the branch in local repository. User with <b><i>git branch -r</i></b> to view list of branch 
+                        in remote. There are more branch argument availale....
                       </p>
                       <p className={article.highlightText} style={style.highlight}>
-                        <b>Syntax: </b><br />
-                        dateObj.toLocaleDateString(<i>[locales[, options]]</i>)
+                        <b>git checkout [branch]</b>
                       </p>
-                      <center>
-                        <GridItem xs={12} sm={12} md={10}>
-                          <Gist id="4981cff6cb21818814e5b7c435d0b005" file="LocaleOptions.js" />
-                        </GridItem>
-                      </center>
-                      <p>More info about locale parameter =&#62;
-                        <a target='_blank' rel="noopener noreferrer"
-                          href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat'>here</a>
-                      </p>
-                      <p className={article.text} style={style.highlight}>
-                        <b>Note: </b> <br />
-                        If you dont specify the some of the property in options, it wont display
-                      </p>
-                    </GridItem>
-
-                    <GridItem xs={12} sm={12} md={10}>
-                      <hr />
-                      <h3>Constructor of Date</h3>
-                      <p className={article.text} style={style.highlight}>
-                        Before we manipulating and formating the datetime, we need to change the string to the date
-                        object. We can use the date constructor to make our date string into date object.
-                      </p>
-                      <center>
-                        <GridItem xs={12} sm={12} md={10}>
-                          <Gist id="4981cff6cb21818814e5b7c435d0b005" file="DateConstructor.js" />
-                        </GridItem>
-                      </center>
-                      <p>More info about date constructor =&#62;
-                        <a target='_blank' rel="noopener noreferrer"
-                          href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date'>here</a>
-                      </p>
-                    </GridItem>
-
-                    <GridItem xs={12} sm={12} md={10}>
-                      <hr />
-                      <h3>Get specific value from the date object</h3>
-                      <p className={article.text} style={style.highlight}>
-                        In order to get the specific value of the date such as such as the year, month, date and time.
-                        Just use the get method will do.
-                      </p>
-                      <center>
-                        <GridItem xs={12} sm={12} md={10}>
-                          <Gist id="4981cff6cb21818814e5b7c435d0b005" file="GetValue.js" />
-                        </GridItem>
-                        <p>More get method =&#62;
-                        <a target='_blank' rel="noopener noreferrer"
-                            href='https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date'>here</a>
-                        </p>
-                      </center>
-                    </GridItem>
-
-                    <GridItem xs={12} sm={12} md={10}>
-                      <hr />
-                      <h3>Tricks in manipulating datetime format</h3>
-                      <p className={article.text} style={style.highlight}>
-                        If you are not using any javascript datetime library. The tricks below could
-                        help you a lot.
+                      <p className={article.text}>
+                        <b>Explanation: </b>
+                        Switched to another branch. If use with <b><i>git checkout -b [branch]</i></b> will create the branch if it 
+                        is not exist. If branch is not in local yet (in remote) use git pull to pull down the branch then only 
+                        checkout to switch to it.
                       </p>
                       <p className={article.highlightText} style={style.highlight}>
-                        <b>1. Get Name of Month and Day</b><br />
-                        Since <i>getMonth()</i> and <i>getDay()</i> method return only number value, there are
-                        two different ways to get the name of the month or day. First is using the appropriate way
-                        (can choose short or long name) while second is quick hack to get short name of day or month.
+                        <b>git merge [branch]</b>
                       </p>
-                      <center>
-                        <GridItem xs={12} sm={12} md={10}>
-                          <Gist id="4981cff6cb21818814e5b7c435d0b005" file="GetDayMonthName.js" />
-                        </GridItem>
-                      </center>
-                      <p className={article.highlightText} style={style.highlight}>
-                        <b>2. Add or substract day</b><br />
-                        In order to add or substract number of day to the date. First convert your date object into
-                        milliseconds by using <i>Date.parse() </i> or <i>getTime() </i>. Both of these function will
-                        returns the number of milliseconds since January 1, 1970, 00:00:00 UTC. After that add the value
-                        according to your desired value where 1 second = 1000 milliseconds. Last, convert the milliseconds
-                        back to the date object using the date constructor.
+                      <p className={article.text}>
+                        <b>Explanation: </b>
+                        Merge branch into current branch. If you current pointing at the master branch, 
+                        <b><i> git merge [branch]</i></b> will merge the <b><i>[branch]</i></b> into master branch.
                       </p>
-                      <center>
-                        <GridItem xs={12} sm={12} md={10}>
-                          <Gist id="4981cff6cb21818814e5b7c435d0b005" file="AddDay.js" />
-                        </GridItem>
-                      </center>
-                      <p className={article.highlightText} style={style.highlight}>
-                        <b>3. Comparing dates</b><br />
-                        Same with add or substract day. Convert the date into milliseconds by using <i>Date.parse() </i>
-                        or <i>getTime() </i>. After that, compare it by using the compare operator.
-                      </p>
-                      <center>
-                        <GridItem xs={12} sm={12} md={10}>
-                          <Gist id="4981cff6cb21818814e5b7c435d0b005" file="ComparingDate.js" />
-                        </GridItem>
-                      </center>
                     </GridItem>
 
                     <GridItem xs={12} sm={12} md={10}>
                       <hr />
-                      <h3>Javascript datetime library</h3>
+                      <h3>Others...</h3>
                       <p className={article.text} style={style.highlight}>
                         When you need a complex manipulation on the datetime format, a javascript datetime library will
                         make ease of you because the current Javascript datetime capabilities is not that great.
